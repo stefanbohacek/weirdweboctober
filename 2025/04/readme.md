cf762ed2b85337e756ea31f25c5518cf4cf9dc5b
Stefan Bohacek <stefan@stefanbohacek.com>
2025-10-04 10:06:17 -0400

    Added project 4.

---
+++ 2025/04/readme.md
@@ -0,0 +1,5 @@
+Image credit: 
+
+Rodion Kutsaiev: https://unsplash.com/photos/a-foggy-graveyard-with-tombstones-in-the-foreground-gYC68k9trV8
+
+The Skeleton Dance (1929) https://en.wikipedia.org/wiki/The_Skeleton_Dance
--- 2025/04/scripts.js
+++ 2025/04/scripts.js
@@ -1,9 +1,11 @@
-const slider = document.getElementById("opacitySlider");
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
 const topImage = document.getElementById("top-image");
 const opacityValue = document.getElementById("opacityValue");
 
 slider.addEventListener("input", function () {
   const opacity = this.value / 100;
+  sliderValue.innerHTML = this.value
   topImage.style.opacity = opacity;
   opacityValue.textContent = this.value;
 });
